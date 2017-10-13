4131160a8aa783db4f0da12c8085543f4e5baa65
Sebastian Freundt <freundt@ga-group.nl>
2017-10-13 08:33:50 +0000

    minor, migrate to murmur3 for hashing

--- src/nquads-fold.c
+++ src/nquads-fold.c
@@ -39,12 +39,11 @@
 #endif	/* HAVE_CONFIG_H */
 #include <unistd.h>
 #include <stdbool.h>
-#include <stddef.h>
+#include <stdint.h>
 #include <stdio.h>
 #include <string.h>
 #include <stdarg.h>
 #include <errno.h>
-#include "fnv.h"
 #include "nifty.h"
 
 
@@ -72,24 +71,122 @@ c2h(unsigned int c)
 }
 
 
+/* murmur3 */
+#define HASHSIZE	(128U / 8U)
+
+static inline __attribute__((pure, const)) uint64_t
+rotl64(uint64_t x, int8_t r)
+{
+	return (x << r) | (x >> (64 - r));
+}
+
+static inline __attribute__((pure, const, always_inline)) uint64_t
+fmix64(uint64_t k)
+{
+	k ^= k >> 33;
+	k *= 0xff51afd7ed558ccdULL;
+	k ^= k >> 33;
+	k *= 0xc4ceb9fe1a85ec53ULL;
+	k ^= k >> 33;
+
+	return k;
+}
+
+static void
+MurmurHash3_x64_128(const void *key, size_t len, uint8_t out[static HASHSIZE])
+{
+	const uint8_t *data = (const uint8_t*)key;
+	const size_t nblocks = len / 16U;
+
+	uint64_t h1 = 0U, h2 = 0U;
+
+	const uint64_t c1 = 0x87c37b91114253d5ULL;
+	const uint64_t c2 = 0x4cf5ad432745937fULL;
+
+	//----------
+	// body
+
+	const uint64_t * blocks = (const uint64_t *)(data);
+
+	for (size_t i = 0; i < nblocks; i++) {
+		uint64_t k1 = blocks[i*2+0];
+		uint64_t k2 = blocks[i*2+1];
+
+		k1 *= c1; k1  = rotl64(k1,31); k1 *= c2; h1 ^= k1;
+
+		h1 = rotl64(h1,27); h1 += h2; h1 = h1*5+0x52dce729;
+
+		k2 *= c2; k2  = rotl64(k2,33); k2 *= c1; h2 ^= k2;
+
+		h2 = rotl64(h2,31); h2 += h1; h2 = h2*5+0x38495ab5;
+	}
+
+	//----------
+	// tail
+
+	const uint8_t * tail = (const uint8_t*)(data + nblocks*16);
+
+	uint64_t k1 = 0;
+	uint64_t k2 = 0;
+
+	switch(len & 15)
+	{
+	case 15: k2 ^= ((uint64_t)tail[14]) << 48;
+	case 14: k2 ^= ((uint64_t)tail[13]) << 40;
+	case 13: k2 ^= ((uint64_t)tail[12]) << 32;
+	case 12: k2 ^= ((uint64_t)tail[11]) << 24;
+	case 11: k2 ^= ((uint64_t)tail[10]) << 16;
+	case 10: k2 ^= ((uint64_t)tail[ 9]) << 8;
+	case  9: k2 ^= ((uint64_t)tail[ 8]) << 0;
+		k2 *= c2; k2  = rotl64(k2,33); k2 *= c1; h2 ^= k2;
+
+	case  8: k1 ^= ((uint64_t)tail[ 7]) << 56;
+	case  7: k1 ^= ((uint64_t)tail[ 6]) << 48;
+	case  6: k1 ^= ((uint64_t)tail[ 5]) << 40;
+	case  5: k1 ^= ((uint64_t)tail[ 4]) << 32;
+	case  4: k1 ^= ((uint64_t)tail[ 3]) << 24;
+	case  3: k1 ^= ((uint64_t)tail[ 2]) << 16;
+	case  2: k1 ^= ((uint64_t)tail[ 1]) << 8;
+	case  1: k1 ^= ((uint64_t)tail[ 0]) << 0;
+		k1 *= c1; k1  = rotl64(k1,31); k1 *= c2; h1 ^= k1;
+	};
+
+	//----------
+	// finalization
+
+	h1 ^= len; h2 ^= len;
+
+	h1 += h2;
+	h2 += h1;
+
+	h1 = fmix64(h1);
+	h2 = fmix64(h2);
+
+	h1 += h2;
+	h2 += h1;
+
+	((uint64_t*)out)[0] = h1;
+	((uint64_t*)out)[1] = h2;
+}
+
+
 static int
 fold1(FILE *fp)
 {
-	unsigned char H[2U * FNV256size + 1U] = {
-		[2U * FNV256size] = '\n'
+	unsigned char H[2U * HASHSIZE + 1U] = {
+		[2U * HASHSIZE] = '\n'
 	};
 	char *line = NULL;
 	size_t llen = 0UL;
 
 	for (ssize_t nrd; (nrd = getline(&line, &llen, fp)) > 0;) {
-		uint8_t h[FNV256size];
+		uint8_t h[HASHSIZE];
 
 		nrd -= line[nrd - 1] == '\n';
 		line[nrd] = '\0';
 
-		if (UNLIKELY(FNV256block(line, nrd, h) < 0)) {
-			memset(h, 0, sizeof(h));
-		}
+		MurmurHash3_x64_128(line, nrd, h);
+
 		/* print hash */
 		for (size_t i = 0U; i < countof(h); i++) {
 			H[2U * i + 0U] = c2h((h[i] >> 0U) & 0b1111U);
