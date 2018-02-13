6b43b3294220b115f308b609e2f12e5ca1c8a99d
Sebastian Freundt <freundt@ga-group.nl>
2018-02-13 06:03:16 +0000

    minor, allow "@PREFIX" and <@PREFIX> nodes that are replaced by @prefix definitions

--- src/metarap.c
+++ src/metarap.c
@@ -1,8 +1,55 @@
+/*** metarap.c -- reify rdf
+ *
+ * Copyright (C) 2017-2018 Sebastian Freundt
+ *
+ * Author:  Sebastian Freundt <freundt@ga-group.nl>
+ *
+ * This file is part of rdfsnips.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of the author nor the names of any contributors
+ *    may be used to endorse or promote products derived from this
+ *    software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR "AS IS" AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
+ * BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+ * OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
+ * IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ ***/
 #include <stdlib.h>
 #include <string.h>
 #include <stdint.h>
 #include <raptor2.h>
 #include "nifty.h"
+
+
+static void*
+recalloc(void *oldp, size_t oldz, size_t newz, size_t nmemb)
+{
+	char *p = realloc(oldp, newz * nmemb);
+	if (LIKELY(newz >= oldz)) {
+		memset(p + oldz * nmemb, 0, (newz - oldz) * sizeof(nmemb));
+	}
+	return p;
+}
 
 
 /* murmur3 */
@@ -122,6 +169,63 @@ mmh3(unsigned char *restrict tgt, size_t tsz, const char *str, size_t len)
 	return 2U * HASHSIZE;
 }
 
+static char*
+xmemmem(const char *hay, const size_t hayz, const char *ndl, const size_t ndlz)
+{
+	const char *const eoh = hay + hayz;
+	const char *const eon = ndl + ndlz;
+	const char *hp;
+	const char *np;
+	const char *cand;
+	unsigned int hsum;
+	unsigned int nsum;
+	unsigned int eqp;
+
+	/* trivial checks first
+         * a 0-sized needle is defined to be found anywhere in haystack
+         * then run strchr() to find a candidate in HAYSTACK (i.e. a portion
+         * that happens to begin with *NEEDLE) */
+	if (ndlz == 0UL) {
+		return deconst(hay);
+	} else if ((hay = memchr(hay, *ndl, hayz)) == NULL) {
+		/* trivial */
+		return NULL;
+	}
+
+	/* First characters of haystack and needle are the same now. Both are
+	 * guaranteed to be at least one character long.  Now computes the sum
+	 * of characters values of needle together with the sum of the first
+	 * needle_len characters of haystack. */
+	for (hp = hay + 1U, np = ndl + 1U, hsum = *hay, nsum = *hay, eqp = 1U;
+	     hp < eoh && np < eon;
+	     hsum ^= *hp, nsum ^= *np, eqp &= *hp == *np, hp++, np++);
+
+	/* HP now references the (NZ + 1)-th character. */
+	if (np < eon) {
+		/* haystack is smaller than needle, :O */
+		return NULL;
+	} else if (eqp) {
+		/* found a match */
+		return deconst(hay);
+	}
+
+	/* now loop through the rest of haystack,
+	 * updating the sum iteratively */
+	for (cand = hay; hp < eoh; hp++) {
+		hsum ^= *cand++;
+		hsum ^= *hp;
+
+		/* Since the sum of the characters is already known to be
+		 * equal at that point, it is enough to check just NZ - 1
+		 * characters for equality,
+		 * also CAND is by design < HP, so no need for range checks */
+		if (hsum == nsum && memcmp(cand, ndl, ndlz - 1U) == 0) {
+			return deconst(cand);
+		}
+	}
+	return NULL;
+}
+
 
 struct buf_s {
 	char *s;
@@ -212,6 +316,97 @@ nmsp(void *user_data, raptor_namespace *ns)
 }
 
 
+/* replacement of "@PREFIX" and <@PREFIX> */
+static unsigned char *rplc;
+static size_t nrplc;
+static size_t zrplc;
+
+static size_t **cord;
+static size_t zcord;
+static size_t *ncord;
+static size_t *sufxs;
+static raptor_term ***cake;
+
+static void
+free_rplc(void)
+{
+	for (size_t i = 0U; i < zcord; i++) {
+		free(cord[i]);
+	}
+	for (size_t i = 0U; i < zcord; i++) {
+		for (size_t j = 0U; j < ncord[i]; j++) {
+			raptor_free_term(cake[i][j]);
+		}
+		free(cake[i]);
+	}
+	free(cord);
+	free(ncord);
+	free(cake);
+	free(sufxs);
+
+	free(rplc);
+	return;
+}
+
+static size_t
+find_rplc(const unsigned char *str, size_t len)
+{
+	const char *p;
+	size_t r;
+
+	if (!(p = xmemmem((const char*)rplc, nrplc, (const char*)str, len))) {
+		return -1ULL;
+	} else if (p[-1] != '@' || p[len]) {
+		return -1ULL;
+	}
+	r = ((const unsigned char*)p - rplc);
+	return r / sizeof(r);
+}
+
+static size_t
+add_rplc(const unsigned char *str, size_t len)
+{
+/* register prefix STR (sans the @) of size LEN
+ * we align the strings on a sizeof(size_t) boundary */
+	size_t r = nrplc;
+
+	if (UNLIKELY(nrplc + len + sizeof(r) >= zrplc)) {
+		while ((zrplc *= 2U) < nrplc + len + sizeof(r));
+		rplc = realloc(rplc, zrplc);
+	}
+	rplc[nrplc++] = '@';
+	memcpy(rplc + nrplc, str, len);
+	nrplc += len;
+	/* fast forward to next boundary */
+	memset(rplc + nrplc, 0, sizeof(r) - (nrplc % sizeof(r)));
+	nrplc += sizeof(r) - (nrplc % sizeof(r));
+	return r / sizeof(r);
+}
+
+static void
+add_cord(const size_t r, size_t i, size_t j, raptor_term *t, size_t sufx)
+{
+	if (UNLIKELY(r >= zcord)) {
+		const size_t nuz = (zcord * 2U) ?: 64U;
+		cord = recalloc(cord, zcord, nuz, sizeof(*cord));
+		cake = recalloc(cake, zcord, nuz, sizeof(*cake));
+		ncord = recalloc(ncord, zcord, nuz, sizeof(*ncord));
+		sufxs = recalloc(sufxs, zcord, nuz, sizeof(*sufxs));
+		zcord = nuz;
+	}
+	if (UNLIKELY(!(ncord[r] & (ncord[r] + 2U)))) {
+		const size_t nuz = (ncord[r] + 2U) * 2U;
+		cord[r] = realloc(cord[r], nuz * sizeof(*cord[r]));
+		cake[r] = realloc(cake[r], nuz * sizeof(*cake[r]));
+	}
+	cake[r][ncord[r] / 2U] = raptor_term_copy(t);
+	cord[r][ncord[r]++] = i;
+	cord[r][ncord[r]++] = j;
+	sufxs[r] = (sufx > sufxs[r]) ? sufx : sufxs[r];
+	return;	
+}
+
+
 static raptor_world *world;
 static raptor_uri *base;
 static raptor_term *rdfssub, *rdfspred, *rdfsobj;
@@ -224,7 +419,101 @@ static size_t nterms;
 static size_t zterms;
 static raptor_term ***beefs;
 static size_t *nbeefs;
-static size_t *zbeefs;
+
+static void
+free_terms(void)
+{
+	for (size_t i = 0U; i < nterms; i++) {
+		for (size_t j = 0U; j < nbeefs[i]; j++) {
+			raptor_free_term(beefs[i][j]);
+		}
+		free(beefs[i]);
+	}
+	free(beefs);
+	free(nbeefs);
+
+	for (size_t i = 0U; i < nterms; i++) {
+		raptor_free_term(terms[i]);
+	}
+	free(terms);
+	return;
+}
+
+static void
+nscp(void *user_data, raptor_namespace *ns)
+{
+	raptor_serializer *UNUSED(sfold) = user_data;
+	unsigned char *uristr;
+	size_t urilen;
+	const unsigned char *prestr;
+	size_t prelen;
+	size_t r;
+
+	prestr = raptor_namespace_get_counted_prefix(ns, &prelen);
+	if (!~(r = find_rplc(prestr, prelen))) {
+		return;
+	}
+	with (raptor_uri *uri = raptor_namespace_get_uri(ns)) {
+		uristr = raptor_uri_as_counted_string(uri, &urilen);
+	}
+	/* a service for the suffixing later on */
+	unsigned char tmp[urilen + sufxs[r]];
+	if (sufxs[r]) {
+		memcpy(tmp, uristr, urilen);
+		uristr = tmp;
+	}
+	for (size_t k = 0U; k < ncord[r]; k += 2U) {
+		/* replace terms */
+		const size_t i = cord[r][k + 0U];
+		const size_t j = cord[r][k + 1U];
+		const raptor_term *proto = cake[r][k / 2U];
+		size_t tmplen;
+
+		switch (proto->type) {
+			const unsigned char *prostr;
+			size_t prolen;
+			raptor_term *t;
+
+		case RAPTOR_TERM_TYPE_URI:
+			prostr = raptor_uri_as_counted_string(
+				proto->value.uri, &prolen);
+			goto sufxchck;
+
+		case RAPTOR_TERM_TYPE_LITERAL:
+			prostr = proto->value.literal.string;
+			prolen = proto->value.literal.string_len;
+			goto sufxchck;
+
+		sufxchck:
+			tmplen = urilen;
+			if (prelen < prolen && prostr[prelen + 1U] == ':') {
+				memcpy(uristr + urilen,
+				       prostr + (prelen + 2U),
+				       prolen - (prelen + 2U));
+				tmplen += prolen - (prelen + 2U);
+			}
+			switch (proto->type) {
+			case RAPTOR_TERM_TYPE_URI:
+				t = raptor_new_term_from_counted_uri_string(
+					world, uristr, tmplen);
+				break;
+			case RAPTOR_TERM_TYPE_LITERAL:
+				t = raptor_new_term_from_counted_literal(
+					world, uristr, tmplen,
+					proto->value.literal.datatype,
+					proto->value.literal.language,
+					proto->value.literal.language_len);
+				break;
+			}
+			/* actually do the replacing now */
+			raptor_free_term(beefs[i][j]);
+			beefs[i][j] = t;
+		default:
+			break;
+		}
+	}
+	return;
+}
 
 static void
 prnt(void *user_data, raptor_statement *triple)
@@ -295,25 +584,60 @@ flts(void *UNUSED(user_data), raptor_statement *triple)
 	if (UNLIKELY(nterms >= zterms)) {
 		zterms *= 2U;
 		terms = realloc(terms, zterms * sizeof(*terms));
-		beefs = realloc(beefs, zterms * sizeof(*beefs));
-		nbeefs = realloc(nbeefs, zterms * sizeof(*nbeefs));
-		zbeefs = realloc(zbeefs, zterms * sizeof(*zbeefs));
+		beefs = recalloc(beefs, zterms / 2U, zterms, sizeof(*beefs));
+		nbeefs = recalloc(nbeefs, zterms / 2U, zterms, sizeof(*nbeefs));
 	}
 	terms[i] = raptor_term_copy(triple->subject);
-	nbeefs[i] = 0U;
-	zbeefs[i] = 64U;
-	beefs[i] = malloc(zbeefs[i] * sizeof(*beefs));
 	nterms++;
 
 yep:
 	/* bang po to beefs */
-	if (UNLIKELY(nbeefs[i] >= zbeefs[i])) {
+	if (UNLIKELY(!(nbeefs[i] & (nbeefs[i] + 2U)))) {
 		/* resize */
-		zbeefs[i] *= 2U;
-		beefs[i] = realloc(beefs[i], zbeefs[i] * sizeof(*beefs[i]));
+		const size_t nuz = (nbeefs[i] + 2U) * 2U;
+		beefs[i] = realloc(beefs[i], nuz * sizeof(*beefs[i]));
 	}
 	beefs[i][nbeefs[i]++] = raptor_term_copy(triple->predicate);
 	beefs[i][nbeefs[i]++] = raptor_term_copy(triple->object);
+
+	/* see if triple->object contains @PREFIX semantics */
+	switch (triple->object->type) {
+		const unsigned char *str;
+		const unsigned char *eos;
+		size_t len;
+		size_t r;
+
+	case RAPTOR_TERM_TYPE_URI:
+		str = raptor_uri_as_counted_string(
+			triple->object->value.uri, &len);
+		goto chck;
+	case RAPTOR_TERM_TYPE_LITERAL:
+		str = triple->object->value.literal.string;
+		len = triple->object->value.literal.string_len;
+		goto chck;
+
+	chck:
+		if (len--, *str++ != '@') {
+			break;
+		}
+		eos = memchr(str, ':', len) ?: str + len;
+		/* see if we've got him */
+		if (!~(r = find_rplc(str, eos - str))) {
+			/* nope, add him then */
+			r = add_rplc(str, eos - str);
+		}
+		/* bang coords and keep original object term */
+		{
+			const size_t j = nbeefs[i] - 1U;
+			raptor_term *t = beefs[i][j];
+			const size_t sufx = str + len - eos;
+			add_cord(r, i, j, t, sufx);
+		}
+		break;
+
+	default:
+		break;
+	}
 	return;
 }
 
@@ -332,8 +656,9 @@ fltr(const char *fn, raptor_serializer *sfold)
 		terms = malloc(zterms * sizeof(*terms));
 		beefs = calloc(zterms, sizeof(*beefs));
 		nbeefs = calloc(zterms, sizeof(*nbeefs));
-		zbeefs = calloc(zterms, sizeof(*zbeefs));
 	}
+
+	rplc = malloc(zrplc = 256U);
 
 	raptor_parser_set_statement_handler(p, NULL, flts);
 	raptor_parser_set_namespace_handler(p, sfold, nmsp);
@@ -412,6 +737,7 @@ main(int argc, char *argv[])
 		}
 		raptor_parser_set_statement_handler(
 			p, &(struct ctx_s){&buf, shash, sfold}, prnt);
+		raptor_parser_set_namespace_handler(p, sfold, nscp);
 		raptor_parser_parse_file_stream(p, stdin, NULL, base);
 		raptor_free_parser(p);
 	}
@@ -435,19 +761,8 @@ err:
 	raptor_free_iostream(iostream);
 
 	/* unfilter */
-	for (size_t i = 0U; i < nterms; i++) {
-		raptor_free_term(terms[i]);
-	}
-	free(terms);
-	for (size_t i = 0U; i < nterms; i++) {
-		for (size_t j = 0U; j < nbeefs[i]; j++) {
-			raptor_free_term(beefs[i][j]);
-		}
-		free(beefs[i]);
-	}
-	free(beefs);
-	free(nbeefs);
-	free(zbeefs);
+	free_terms();
+	free_rplc();
 
 	raptor_free_world(world);
 
